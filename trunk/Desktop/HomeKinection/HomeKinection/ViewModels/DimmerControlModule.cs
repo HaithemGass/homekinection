81776372c035a8402ea7be5422bca7233f3332a3
bowman.brian.a <bowman.brian.a@42c3bba0-0376-d72e-e3f3-3e074fc1e219>
2011-11-14 02:41:32 +0000

    Added in code to make a list of modules and allow for manual control.  Currently we only have shade and dimmer.

---
+++ trunk/Desktop/HomeKinection/HomeKinection/ViewModels/DimmerControlModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace HomeKinection
+{
+    public class DimmerControlModule : ModuleBox
+	{
+        private int inten;
+		public NetworkProtocol network;
+		private NetworkProtocol.DimmerCommandData packet;
+		public int intensity{
+            get
+            {
+                return inten;
+            }
+            set
+            {
+                inten = value;
+				packet.intensity = System.Convert.ToByte(value);
+				if(network != null) network.sendDimmerMessage(address, packet);
+                NotifyPropertyChanged("intensity");
+            }						
+        }
+		
+		
+		public DimmerControlModule()
+		{
+		}
+		public DimmerControlModule(NetworkProtocol net, String n, String l, UInt16 sa, UInt64 u, MODULE_TYPE t)
+		{
+            network = net;
+            name = n;
+            location = l;
+            address = sa;
+            uid = u;
+            type = t;
+
+		}
+	}
+}
---
+++ trunk/Desktop/HomeKinection/HomeKinection/Views/DimmerControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeKinection
+{	
+	/// <summary>
+	/// Interaction logic for DimmerControl.xaml
+	/// </summary>
+	public partial class DimmerControl : UserControl
+	{
+		public DimmerControl(NetworkProtocol net, String n, String l, UInt16 sa, UInt64 u, MODULE_TYPE t)
+		{
+			this.InitializeComponent();
+			DimmerControlModule dc = (DimmerControlModule)this.FindResource("dataModel");
+			
+			dc.address = sa;			
+			dc.name = n;
+			dc.network = net;
+			dc.location = l;
+			dc.uid = u;
+			dc.type = t;
+			
+		}
+	
+	}
+}
